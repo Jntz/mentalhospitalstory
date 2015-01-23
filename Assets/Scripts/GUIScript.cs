3c2c99c462e508087f9695d149e03fe545712307
Joni Ahola <joni@juotos.fi>
2015-01-23 21:51:24 +0200

    Organize logic to functions in GUIScript

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -4,23 +4,36 @@ using System.Collections;
 public class GUIScript : MonoBehaviour {
 
 	public Texture[] textures;
+	private Texture currentTexture; //texture which use in background
 	public Rect optionRect1, optionRect2, optionRect3;
+	public string optionText1, optionText2, optionText3;
+	public GUIStyle buttonStyle;
+
+	//Static definitions
+	private float width = 100f, height = 30f, buttonStartingPos = 0f;
+
 
 	// Use this for initialization
 	void Start () {
-		initializeOptionRects ();
+		buttonStartingPos = Screen.height - (3 * (height + 15f));
+		initializeOptions ();
 		loadAllBackground ();
+		currentTexture = textures [0];
 	}
 
-	void initializeOptionRects() {
-		float width = 100f, height = 30f;
-		float x = 20f, y = Screen.height - (4* (height + 15f));
+	void initializeOptions() {
+		float x = 20f, y = buttonStartingPos;
 
 		optionRect1 = new Rect (x, y, width, height);
 		y += height + 15f;
+		optionText1 = "Teksti1";
+
 		optionRect2 = new Rect (x, y, width, height);
 		y += height + 15f;
+		optionText2 = "Teksti2";
+
 		optionRect3 = new Rect (x, y, width, height);
+		optionText3 = "Teksti3";
 	}
 
 
@@ -32,26 +45,28 @@ public class GUIScript : MonoBehaviour {
 			textures[i] = obj_textures[i] as Texture;
 		}
 	}
-
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void selectOption1() {
+		Debug.Log ("Press button one");		
 	}
+	void selectOption2() {
+		Debug.Log ("Press button two");
+	}
+	void selectOption3() {
+		Debug.Log ("Press button three");
+	}
+
 	void OnGUI() {
-		//Scene: 1 background kuva, 3-4 vaihtoehtoa josta vaihtuu seuraavaan skeneen ja pitää tallentaa pelaajan eteneminen muistiin
-		GUI.DrawTexture (new Rect (0f, 0f, Screen.width, Screen.height), textures[0], ScaleMode.ScaleToFit);
-
-		//kolme vaihtoehtoa 1 2 3 
-		if (GUI.Button (optionRect1, "test")) {
-			Debug.Log ("Painoit ykköstä");		
+		GUI.DrawTexture (new Rect (0f, 0f, Screen.width, Screen.height), currentTexture, ScaleMode.ScaleToFit); //texture fill camera area
+					
+		if (GUI.Button (optionRect1, optionText1)) {
+			selectOption1();
 		}  
-		if (GUI.Button (optionRect2, "test2")) {
-			Debug.Log ("Painoit kakkosta");
+		if (GUI.Button (optionRect2, optionText2)) {
+			selectOption2();
 		}
-		if (GUI.Button (optionRect3, "test3")) {
-			Debug.Log ("Painoit kolmosta");
+		if (GUI.Button (optionRect3, optionText3)) {
+			selectOption3();
 		}
-
 	}
 }
