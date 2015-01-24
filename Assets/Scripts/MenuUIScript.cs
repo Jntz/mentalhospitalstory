2c28c2ce022375ad495866bfe1686a20c884ce96
Joni Ahola <joni@juotos.fi>
2015-01-24 23:39:12 +0200

    Add UI logic to menuscene

---
+++ Assets/Scripts/MenuUIScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuUIScript : MonoBehaviour {
+
+	public void MoveToGame() {
+		Application.LoadLevel (1);
+	}
+}
